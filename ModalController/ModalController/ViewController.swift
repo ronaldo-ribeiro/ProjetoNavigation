db1f84c0c353737a2b20a54499db657fade6f0cd
Ronaldo Ribeiro <ronaldo_ribeiro@outlook.com>
2022-10-16 11:52:54 -0300

    Finalização do Desafio 2 - Exibir uma modal com segue sendo referenciada pela controller

--- ModalController/ModalController/ViewController.swift
+++ ModalController/ModalController/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func GoTela01(_ sender: UIButton) {
+        performSegue(withIdentifier: "GoTela01", sender: nil)
+    }
+    
 }
-
